023ddbe0550e6925ff8add0b50bb898557a41bcd
arturhonores <luis.arturhonores@gmail.com>
2024-08-29 23:07:42 +0200

    migrations ok

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 
 class Category extends Model
 {
     use HasFactory;
+
+    //Relacion de uno a muchos con post
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
 
 class Post extends Model
 {
     use HasFactory;
+
+    //relación uno a muchos inversa
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    //relación muchos a muchos
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
