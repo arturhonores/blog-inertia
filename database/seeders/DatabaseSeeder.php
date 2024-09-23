428d7206dc02dff7da5dc358bf48badd93c09a4b
arturhonores <luis.arturhonores@gmail.com>
2024-09-23 10:41:58 +0200

    seeder condicional

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,11 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Tag;
+use App\Models\Author;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,13 +16,36 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
+    // {
+    //     $this->call([
+    //         UserSeeder::class,
+    //         CategorySeeder::class,
+    //         TagSeeder::class,
+    //         AuthorSeeder::class,
+    //         PostSeeder::class
+    //     ]);
+    // }
+    //seeder condicionales
     {
-        $this->call([
-            UserSeeder::class,
-            CategorySeeder::class,
-            TagSeeder::class,
-            AuthorSeeder::class,
-            PostSeeder::class
-        ]);
+        if (User::count() == 0) {
+            $this->call(UserSeeder::class);
+        }
+
+        if (Category::count() == 0) {
+            $this->call(CategorySeeder::class);
+        }
+
+        if (Tag::count() == 0) {
+            $this->call(TagSeeder::class);
+        }
+
+        if (Author::count() == 0) {
+            $this->call(AuthorSeeder::class);
+        }
+
+        // Opcionalmente, si quieres asegurarte de que haya algunos posts iniciales:
+        if (Post::count() == 0) {
+            $this->call(PostSeeder::class);
+        }
     }
 }
