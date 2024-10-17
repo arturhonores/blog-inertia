734d181e662c18a14ccc6b3bb5b9936082931ac3
arturhonores <luis.arturhonores@gmail.com>
2024-10-17 11:07:30 +0200

    pagination error htpps intento 3 configurando app.php

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,14 +3,3 @@ window.axios = axios;
 
 // Configurar el encabezado por defecto para las peticiones XMLHttpRequest
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-
-//código adicional por problemas con el https
-
-// Detectar si estamos en producción
-const isProduction = import.meta.env.PROD;
-
-// Configurar la URL base de Axios
-window.axios.defaults.baseURL = isProduction
-    ? 'https://' + window.location.host
-    : window.location.origin;
